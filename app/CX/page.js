cd66080c947f29bc68067a5960582885822c7a4e
malebosambo <malebo.sambo@outlook.com>
2024-07-07 14:33:40 +0200

    update dialogflow es design, home page, cx page

---
+++ app/CX/page.js
@@ -0,0 +1,11 @@
+export default function CX() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Dialogflow CX</h1>
+        <hr /><br />
+        <p>The chatbot on this page has been implemented using an iframe.</p>
+      </div>
+    </main>
+  );
+}
