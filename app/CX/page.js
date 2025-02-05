20158756a281574b5c8111d032b3b112bd55c14e
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-02-05 15:06:43 +0200

    Update page.js

--- app/CX/page.js
+++ app/CX/page.js
@@ -1,12 +1,23 @@
 export default function CX() {
+
   return (
-    <main className={styles.main}>
+    <main className="Page">
       <div>
         <h1>Dialogflow CX</h1>
         <hr /><br />
         <p>The chatbot on this page has been implemented using an iframe.</p>
       </div>
       <script src="https://www.gstatic.com/dialogflow-console/fast/df-messenger/prod/v1/df-messenger.js"></script>
+      <df-messenger
+        location="europe-west2"
+        project-id="sustained-drake-419306"
+        agent-id="2f4db549-6b4c-4467-ae52-6d6c733bdedd"
+        language-code="en"
+        max-query-length="-1">
+        <df-messenger-chat-bubble
+        chat-title="Shop Assistant">
+        </df-messenger-chat-bubble>
+      </df-messenger>
     </main>
   );
 }
