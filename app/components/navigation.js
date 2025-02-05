9e7bd617044726b3eb6233aebde1fdabe81429dc
Malebo Sambo <71837810+malebosambo@users.noreply.github.com>
2025-02-05 15:08:48 +0200

    Update navigation.js

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,19 +1,15 @@
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function Navigation() {
-  const router = useRouter();
-  const paths = ["/", "/cx", "rest-node-js"];
-  const active = "Navigation_Link Selected";
-  const inactive = "Navigation_Link";
   
   return (
     <>
-      <div className="Navigation">
-        <Link href="/" className={ paths[0] == router.pathname ? active : inactive }>ES</Link>
-        <Link href="/cx" className={ paths[1] == router.pathname ? active : inactive }>CX</Link>
-        <Link href="/rest-node-js" className={ paths[2] == router.pathname ? active : inactive }>NodeJS</Link>
+      <div className="Menu">
+        <Link href="/">ES</Link>
+        <Link href="/cx">CX</Link>
+        <Link href="/nodejs-lib">Node.js</Link>
       </div>
+      
     </>
   )
 }
